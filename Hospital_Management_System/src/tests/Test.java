0c947cf38e039a75ee73aa05bb0282096c6a1d2b
Tuğçe Yavuz <100586556+tugceyavuz@users.noreply.github.com>
2022-05-02 19:03:24 +0300

    Update Test.java

--- Hospital_Management_System/src/tests/Test.java
+++ Hospital_Management_System/src/tests/Test.java
@@ -3,24 +3,23 @@ package tests;
  * @author H. Tuğçe Yavuz
  */
 public class Test {
-  private int patientID;
+ 	private int patientID;
   
-  /**
-  * getter method for patient ID number
-  * @return patient ID number
-  */
-  public int getID(){return patientID;}
+  	/**
+  	* getter method for patient ID number
+  	* @return patient ID number
+  	*/
+  	public int getID(){return patientID;}
   
-  /**
-  * setter method for patient ID number
-  * @param patient ID number
-  */
-  public void setID(int newID){patientID = newID;}
+  	/**
+  	* setter method for patient ID number
+  	* @param patient ID number
+  	*/
+  	public void setID(int newID){patientID = newID;}
   
-  /**
-  *  Constructor For Test Class
-  *  @param patientID number 
-  */
-  public Test(int ID){patientID = ID;}
-
+  	/**
+  	*  Constructor For Test Class
+  	*  @param patientID number 
+  	*/
+  	public Test(int ID){patientID = ID;}
 }
