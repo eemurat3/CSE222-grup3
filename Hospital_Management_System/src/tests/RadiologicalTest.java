040c8080b1714581f3209b81dd315518640212db
S.Burak Yaşar <91782773+4o4x@users.noreply.github.com>
2022-05-03 21:12:00 +0300

    Update RadiologicalTest.java

--- Hospital_Management_System/src/tests/RadiologicalTest.java
+++ Hospital_Management_System/src/tests/RadiologicalTest.java
@@ -1,5 +1,74 @@
 package tests;
 
-public class RadiologicalTest {
+/**
+ * @author S. Burak YASAR
+ */
+
+public class RadiologicalTest extends Test{
+    
+
+    private String testResult;
+
+    /*MR,PET,BT,X-RAY etc. and body parts Example test type: head MR*/
+    private String testType;
+
+    /** Constructor
+    *@param patientID patient id
+    */
+
+    public RadiologicalTest(int patientID){
+        super(patientID);
+    }
+
+    /** Default Constructor*/
+
+    public RadiologicalTest(){super(0);}
+
+    /** Set Result
+    *@param result test result
+    */
+
+    public void setTestResult(String result){
+        testResult = result;
+    }
+
+
+    /** Set Result
+    *@param result test type
+    */
+
+    public void setTestType(String type){
+        testType = type;
+    }
+
+
+
+    /** Get Result
+    *@return returns test result
+    */
+
+    public String getTestResult(){
+        return testResult;
+    }
+
+
+    /** Get Result
+    *@return returns test type
+    */
+
+    public String getTestType(){
+        return testType;
+    }
+
+
+    /** Returns String representation of test result
+    *@return returns test result
+    */
+
+    public String toString(){
+        
+        return "Test Type: " + testType + "\nTest Result: " + testResult;
+    }
+
 
 }
