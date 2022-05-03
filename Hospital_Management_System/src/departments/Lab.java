ace8ebda68b246b9effb47beaee5f860d895f94a
Harun <harunerenozkaya@gmail.com>
2022-05-03 22:14:08 +0300

    Lab class implemented fixed

--- Hospital_Management_System/src/departments/Lab.java
+++ Hospital_Management_System/src/departments/Lab.java
@@ -1,94 +1,91 @@
 package departments;
 /**
  *
- * @author murat Kaymaz
+ * @author harunerenozkaya
  *
  */
-import users.*;
+
 import java.util.ArrayList;
-import java.util.Queue;
-import java.util.PriorityQueue;
-public class Polyclinic extends Department {
-    /**
-     * polyclinic doctor list
-     */
-    private ArrayList<Doctor> doctors = new ArrayList<>();
-    /**
-     * polyclinic nurse list
-     */
-    private ArrayList<Nurse> nurses = new ArrayList<>();
-    /**
-     * polyclinic secreter list
-     */
-    private ArrayList<Secreter> secreters = new ArrayList<>();
-    /***
-     * polyclinic patient queue
-     */
-    private Queue<Patient> patients = new PriorityQueue<>();
-    /**
-     * default constructor
-     */
-    Polyclinic() {
-		/*doctors   = ;
-	    nurses    = new ArrayList<Nurse>();
-	    secreters = new ArrayList<Secreter>();
-	    patients  = new Queue<Patient>();*/
-    }
-    /**
-     * add new doctor to polyclinc
-     * @param d : new doctor
-     */
-    public void addDoctor(Doctor d) {
-        doctors.add(d);
-    }
-    /**
-     * add new nurse to polyclinc
-     * @param n : new nurse
-     */
-    public void addNurse(Nurse n) {
-        nurses.add(n);
-    }
-    /**
-     * add new secreter to polyclinc
-     * @param s : new secreter
-     */
-    public void addSecreter(Secreter s) {
-        secreters.add(s);
-    }
-    /**
-     * add new patients to patients queue
-     * @param p : new patient
-     */
-    public void addPatients(Patient p) {
-        patients.add(p);
-    }
-    /**
-     * get the doctors list of the polyclinic
-     * @return : doctors list
-     */
-    public ArrayList<Doctor> getDoctors() {
-        return doctors;
-    }
-    /**
-     * get the nurses list of the polyclinic
-     * @return : nurses list
-     */
-    public ArrayList<Nurse> getNurses() {
-        return nurses;
-    }
-    /**
-     * get the secreters list of the polyclinic
-     * @return : secreters list
-     */
-    public ArrayList<Secreter> getSecreters() {
-        return secreters;
-    }
-    /**
-     * get the patients queue of the polyclinic
-     * @return : patients Queue
-     */
-    public Queue<Patient>  getPatients() {
-        return patients;
+import java.util.TreeMap;
+
+import tests.Test;
+import users.Technician;
+
+public class Lab extends Department {
+    ArrayList<Technician> clinicalTechnicians;
+    TreeMap<Integer,Test> tests;
+
+
+    public Lab(){
+        clinicalTechnicians = new ArrayList<Technician>();
     }
 
+    public Lab(ArrayList<Technician> clinicalTechnicians){
+        this.clinicalTechnicians = clinicalTechnicians;
+    }
+
+    /**
+     * Get test by test id
+     */
+    public void getTest(Integer testID){
+        tests.get(testID);
+    }
+
+    /**
+     * Add new test
+     */
+    public void addTest(Test test){
+        tests.put(test.getID(), test);
+    }
+
+    /**
+     * Remove a test by object
+     */
+    public void removeTest(Test test){
+        tests.remove(test.getID(),test);
+    }
+
+    /**
+     * Remove a test by testID
+     */
+    public void removeTest(Integer testID){
+        tests.remove(testID);
+    }
+
+    /**
+     * Get clinical technician by technician id
+     */
+    public Technician getClinicalTechnician(Integer technicianID){
+
+        for(Technician i : clinicalTechnicians){
+            if(i.id == technicianID)
+                return i;
+
+        }
+    }
+
+    /**
+     * Add new clinical technician
+     */
+    public void addClinicalTechnician(Technician technician){
+        clinicalTechnicians.add(technician);
+    }
+
+    /**
+     * Remove a clinical technician by object
+     */
+    public void removeClinicalTechnician(Technician technician){
+        clinicalTechnicians.remove(technician);
+    }
+
+    /**
+     * Remove a clinical technician by technician id
+     */
+    public void removeClinicalTechnician(String technicianID){
+        for(int i = 0; i < clinicalTechnicians.size(); i++){
+            if(clinicalTechnicians.get(i).id == technicianID)
+                clinicalTechnicians.remove(i);
+
+        }
+    }
 }
