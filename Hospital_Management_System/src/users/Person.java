1939551d564a7064753887e65af34bd877f2e70b
makifsevilgen <84636599+makifsevilgen@users.noreply.github.com>
2022-05-03 19:29:43 +0300

    Update and rename Person.java to User.java

--- Hospital_Management_System/src/users/Person.java
+++
@@ -1,5 +0,0 @@
-package users;
-
-public class Person {
-
-}
---
+++ Hospital_Management_System/src/users/User.java
@@ -0,0 +1,84 @@
+package users;
+
+import java.util.Objects;
+
+public abstract class User implements Comparable<User>
+{
+	private String name;
+	private String surname;
+	private String id;
+	private int age;
+
+	public User(String name, String surname, String id, int age) {
+        setName(name);
+        setSurname(surname);
+        setId(id);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(User other){
+        return other.getId().compareTo(id);
+    }
+   
+    @Override
+    public String toString() {
+        StringBuilder strB = new StringBuilder();
+        strB.append("Name: ").append(getName());
+        strB.append(", Surname: ").append(getSurname());
+        strB.append(", Id: ").append(getId());
+        strB.append(", Age: ").append(getAge());
+        return strB.toString();
+    }
+}
