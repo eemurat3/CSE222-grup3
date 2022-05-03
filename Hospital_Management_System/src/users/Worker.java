6122f66bb3e8db13ab0de933bc1519a685d0eceb
makifsevilgen <84636599+makifsevilgen@users.noreply.github.com>
2022-05-03 19:40:31 +0300

    Update Worker.java

--- Hospital_Management_System/src/users/Worker.java
+++ Hospital_Management_System/src/users/Worker.java
@@ -1,5 +1,39 @@
 package users;
 
-public class Worker {
+public class Worker extends User
+{
+    private String username;
+    private String password;
 
+    public Worker(String name, String surname, String id, int age,String username , String password) {
+        super(name, surname, id, age);
+        setUsername(username);
+        setPassword(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int compareTo(User other) {
+    	return super.compareTo(other);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Username: " + username;
+    }
 }
