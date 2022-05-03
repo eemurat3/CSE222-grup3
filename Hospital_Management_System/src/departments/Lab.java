a088e28f83e264abda8ace6ce34e1acc808f2cc9
Harun <harunerenozkaya@gmail.com>
2022-05-03 22:06:57 +0300

    Lab class implemented

--- Hospital_Management_System/src/departments/Lab.java
+++ Hospital_Management_System/src/departments/Lab.java
@@ -1,5 +1,48 @@
 package departments;
+import users.*;
+import java.util.*;
 
-public class Lab {
+public class Lab extends Department {
+    ArrayList<Technician> clinicalTechnicians;
 
+
+    public Lab(){
+        clinicalTechnicians = new ArrayList();
+    }
+
+    public Lab(ArrayList<Technician> clinicalTechnicians){
+        this.clinicalTechnicians = clinicalTechnicians;
+    }
+
+    public void getTest(String patientID){
+
+    }
+
+    public void addTest(Test test){
+
+    }
+
+    public void removeTest(Test test){
+
+    }
+
+    public void removeTest(String patientID){
+
+    }
+
+    public void displayClinicalTechnician(String technicianID){
+
+    }
+
+    public void addClinicalTechnician(Technician technician){
+
+    }
+
+    public void removeClinicalTechnician(Technician technician){
+
+    }
+
+    public void removeClinicalTechnician(String technicianID){
+
+    }
 }
