c305fc8f50a1cd85be9469b6e65996ac6ba024f1
S. Burak Yasar <suleymanburakyasar@gmail.com>
2022-05-03 12:30:45 +0300

    Covid Test Result

--- Hospital_Management_System/src/tests/CovidTest.java
+++ Hospital_Management_System/src/tests/CovidTest.java
@@ -1,5 +1,48 @@
 package tests;
 
-public class CovidTest {
+
+/**
+ * @author S. Burak YASAR
+ */
+
+public class CovidTest extends Test{
+    
+    private boolean testResult;
+
+    /** Constructor
+    *@param patientID patient id
+    */
+
+    public CovidTest(int patientID){
+        super(patientID);
+        testResult = true;
+    }
+
+    /** Set Result
+    *@param result test result
+    */
+
+    public void setTestResult(boolean result){
+        testResult = result;
+    }
+
+    /** Get Result
+    *@return returns test result
+    */
+
+    public boolean getTestResult(){
+        return testResult;
+    }
+
+    /** Returns String representation of test result
+    *@return returns test result
+    */
+
+    public String toString(){
+        
+        return   (testResult == true) ? "Covid Test Result: Positive" :
+        "Covid Test Result: Negative";
+    }
+
 
 }
