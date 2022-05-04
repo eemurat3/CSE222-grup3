82f8d45da8c867c2aa07561d16ccadef6fb3a1ee
YeTeA <yusuftalhaaltun@gmail.com>
2022-05-04 21:57:51 +0300

    Manager and Secretary classes are added.

--- Hospital_Management_System/src/users/Manager.java
+++ Hospital_Management_System/src/users/Manager.java
@@ -1,5 +1,52 @@
 package users;
+/**
+ *
+ * @author Yusuf Talha Altun
+ */
+public class Manager extends Worker{
+    public Manager(String name, String surname, String id, int age,String email , String password) {
+        super(name, surname, id, age, email ,  password);
+    }
 
-public class Manager {
+    //Fonksiyon argumani (Worker worker, String newName, String newSurname, ....) olabilir.
+    public void editStaff(Worker worker1, Worker worker2){
+        worker1.setName(worker2.getName());
+        worker1.setSurname(worker2.getSurname());
+        worker1.setID(worker2.getID());
+        worker1.setAge(worker2.getAge());
+        worker1.setUsername(worker2.getUsername());
+        worker1.setPassword(worker2.getPassword());
+    }
+
+    public void removeDepartment(Department department){
+        HMSystem.departments.remove(department);
+    }
+
+    public void inquiryStocks(Product product){
+        product.checkStocks();
+    }
+
+    public void addDepartment(Department department){
+        Department newDep = new Department(department);
+        HMSystem.departments.add(newDep);
+    }
+
+    public void orderProduct(Product product){
+        product.order();
+    }
+
+    public void addStaff(Worker worker){
+        HMSystem.workers.add(worker);
+    }
+
+    public void removeStaff(Worker worker){
+        HMSystem.workers.remove(worker);
+    }
+
+    public void displayPatientInfo(Patient patient){
+        patient.displayPrescriptions();
+        patient.displayDoctors();
+        patient.displayRecords();
+    }
 
 }
--- Hospital_Management_System/src/users/Secreter.java
+++ Hospital_Management_System/src/users/Secreter.java
@@ -1,5 +1,18 @@
 package users;
+/**
+ *
+ * @author Yusuf Talha Altun
+ */
+public class Secreter extends Worker{
+    public Secreter(String name, String surname, String id, int age,String email , String password) {
+        super(name, surname, id, age, email ,  password);
+    }
 
-public class Secreter {
+    public void prescribePatient(Patient patient, Prescription pres , String note) throws IOException{
+        patient.addPrescription(pres);
+    }
 
+    public void displayQueue(Doctor doctor){
+        doctor.waitingPatients();
+    }
 }
