4eaedf7790f73cc81850d9fcccffbd3ddabab62f
Ahmet Usluoglu <ahmet.uslu4155@gmail.com>
2022-05-04 21:51:05 +0300

    prescription eklendi

--- Hospital_Management_System/src/system/Prescription.java
+++ Hospital_Management_System/src/system/Prescription.java
@@ -1,5 +1,25 @@
 package system;
 
-public class Prescription {
+public class Prescription 
+{
+    private String medicine = "None";
+    private String instructions = "None";
+    private String note = "None";
+
+    public Prescription(String newMed, String newInst, String newNote)
+    {
+        medicine = newMed;
+        instructions = newInst;
+        note = newNote;
+    }
+
+    public String getPrescription(){return medicine + instructions + note;}
+    public String getMed(){return medicine;}
+    public String getInstructions(){return instructions;}
+    public String getNote(){return note;}
+
+    public void setMed(String newMed){medicine = newMed;}
+    public void setInstructions(String newInst){instructions = newInst;}
+    public void setNote(String newNote){note = newNote;}
 
 }
