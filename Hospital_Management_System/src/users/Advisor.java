116057a74189925bbbd6b1cbd7a5f0e550f5198c
hasancanonder <67745122+hasancanonder@users.noreply.github.com>
2022-05-05 21:55:18 +0300

    Update Advisor.java

--- Hospital_Management_System/src/users/Advisor.java
+++ Hospital_Management_System/src/users/Advisor.java
@@ -1,22 +1,45 @@
-package users;
+package src;
 
 import java.util.*;
 
 public class Advisor extends Worker {
     
-
     PriorityQueue<Patient> patients = new PriorityQueue<>();
 
 	public Advisor(String name, String surname, String id, int age, String username, String password) {
         super(name, surname, id, age, username, password);
     }
 
-    public void addPatient(Patient nextPatient) {
+    public boolean addPatient(Patient nextPatient) {
+        
+        for (Patient iPatient : patients)
+            if (iPatient.getId() == nextPatient.getId())
+                return false;
+
         patients.add(nextPatient);
+        return true;
     }
 
-    public void removePatient(Patient nextPatient) {
-        patients.remove(nextPatient);
+    public boolean removePatient(Patient nextPatient) {
+        return patients.remove(nextPatient);
+    }
+
+    public String displayQueue() {
+        
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Patients in queue: \n\n");
+
+        for (Patient iPatient : patients) {
+            sb.append("\t" + iPatient + "\n");
+        }
+
+        return new String(sb);
+    }
+
+    @Override
+    public String toString() {
+        return  "Role: Advisor, " + super.toString();
     }
 
 }
