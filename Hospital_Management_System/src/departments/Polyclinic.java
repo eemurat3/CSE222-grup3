aa0189cfafc0e28c30b08431ff8e8b7ebf401e09
eemurat3 <muratkaymaz2538@gmail.com>
2022-05-05 12:26:45 +0300

    Polyclinic code base updated

--- Hospital_Management_System/src/departments/Polyclinic.java
+++ Hospital_Management_System/src/departments/Polyclinic.java
@@ -6,6 +6,7 @@ package departments;
  */
 import users.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.PriorityQueue;
 public class Polyclinic extends Department {
@@ -89,6 +90,123 @@ public class Polyclinic extends Department {
 	 */
 	public Queue<Patient>  getPatients() {
 		return patients;
+	}
+	/**
+	 * Search doctor in the polyclinic doctors list
+	 * @param name : doctor name
+	 * @param surname : doctor surname
+	 * @return : index doctor is find else -1
+	 */
+	public int findDoctor(String name,String surname) {
+		for(int i=0;i<doctors.size();i++) {
+			if(doctors.get(i).getName() == name && doctors.get(i).getSurname()==surname)
+				return i;
+		}
+		return -1;
+	}
+	/**
+	 * Search Nurse in the polyclinic nurses list
+	 * @param name : nurse name
+	 * @param surname : nurse surname
+	 * @return : index nurse is find else -1
+	 */
+	public int findNurse(String name,String surname) {
+		for(int i=0;i<nurses.size();i++) {
+			if(nurses.get(i).getName() == name && nurses.get(i).getSurname()==surname)
+				return i;
+		}
+		return -1;
+	}
+	/**
+	 * Search secreter in the polyclinic secreters list
+	 * @param name : secreter name
+	 * @param surname : secreter surname
+	 * @return : index secreter is find else -1
+	 */
+	public int findSecreter(String name,String surname) {
+		for(int i=0;i<secreters.size();i++) {
+			if(secreters.get(i).getName() == name && secreters.get(i).getSurname()==surname)
+				return i;
+		}
+		return -1;
+	}
+	/**
+	 * Search patient in the polyclinic patients queue
+	 * @param name : patient name
+	 * @param surname : patient surname
+	 * @return : index patient is find else -1
+	 */
+	public int findPatient(String name,String surname) {
+		int index=0;
+		Iterator<Patient> itr = patients.iterator();
+	    while(itr.hasNext()){
+	    	if(itr.next().getName() == name && itr.next().getSurname() == surname) {
+	    		return index;
+	    	}
+	        index++;
+	    }
+		return -1;
+	}
+	/**
+	 * delete existing doctor in the polyclinic doctor list
+	 * @param name : doctor name
+	 * @param surname : doctor surname
+	 * @return null doctor is not finded else deleting doctor object
+	 */
+	public Doctor deleteDoctor(String name,String surname) {
+		int index = findDoctor(name,surname);
+		if(index > 0) {
+			Doctor deleted = doctors.get(index);
+			doctors.remove(index);
+			return deleted;
+		}
+		return null;
+	}
+	/**
+	 * delete existing nurse in the polyclinic nurse list
+	 * @param name : nurse name
+	 * @param surname : nurse surname
+	 * @return  null nurse is not finded else deleting nurse object
+	 */
+	public Nurse deleteNurse(String name,String surname) {
+		int index = findNurse(name,surname);
+		if(index > 0) {
+			Nurse deleted = nurses.get(index);
+			nurses.remove(index);
+			return deleted;
+		}
+		return null;
+	}
+	/**
+	 * delete existing secreter in the polyclinic secreter list
+	 * @param name : secreter name
+	 * @param surname : secreter surname
+	 * @return  null secreter is not finded else deleting secreter object
+	 */
+	public Secreter deleteSecreter(String name,String surname) {
+		int index = findSecreter(name,surname);
+		if(index > 0) {
+			Secreter deleted = secreters.get(index);
+			secreters.remove(index);
+			return deleted;
+		}
+		return null;
+	}
+	/**
+	 * delete existing patient in the polyclinic patient queue
+	 * @param name : patient name
+	 * @param surname : patient surname
+	 * @return  null patient is not finded else deleting patient object
+	 */
+	public Patient deletePatient(String name,String surname) {
+		int index = findPatient(name,surname);
+		if(index > 0) {
+			Patient[] arr = (Patient[]) patients.toArray();
+			Patient deleted = arr[index];
+			patients.remove(deleted);
+			return deleted;
+		}
+		return null;
 	}
 
 }
