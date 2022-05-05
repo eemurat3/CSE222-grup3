ae0850ed5a1e3915f5f329ed778900730728adef
hasancanonder <67745122+hasancanonder@users.noreply.github.com>
2022-05-05 20:42:24 +0300

    Update Advisor.java

--- Hospital_Management_System/src/users/Advisor.java
+++ Hospital_Management_System/src/users/Advisor.java
@@ -1,5 +1,22 @@
 package users;
 
-public class Advisor {
+import java.util.*;
+
+public class Advisor extends Worker {
+    
+
+    PriorityQueue<Patient> patients = new PriorityQueue<>();
+
+	public Advisor(String name, String surname, String id, int age, String username, String password) {
+        super(name, surname, id, age, username, password);
+    }
+
+    public void addPatient(Patient nextPatient) {
+        patients.add(nextPatient);
+    }
+
+    public void removePatient(Patient nextPatient) {
+        patients.remove(nextPatient);
+    }
 
 }
