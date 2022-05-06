3e303ea7281bfdcc2483fb6ea8183d4ed56e8d32
YeTeA <yusuftalhaaltun@gmail.com>
2022-05-07 00:55:39 +0300

    Revert "Revert "update""
    This reverts commit 785af4811a6326d437d3f1f0a8d35fe81884c1b2.

--- Hospital_Management_System/src/unit_tests/ut_polyclinic.java
+++ Hospital_Management_System/src/unit_tests/ut_polyclinic.java
@@ -1,4 +1,6 @@
 package unit_tests;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;	
 import departments.Polyclinic;
 import users.*;
 /**
@@ -10,85 +12,78 @@ public class ut_polyclinic {
 	/**
 	 * Doctor operating test
 	 */
-	//@Test
+	@Test
     static void testDoctorOperating() {
-		System.out.println("******Doctor operation Unit test**********");
 		Polyclinic pl = new Polyclinic();
 		Doctor d = new Doctor("murat","xyz","1",25,"xyz@gtu.uedu.tr","xyz");
 		Doctor d1 = new Doctor("murat2","xyz","1",25,"xyz@gtu.uedu.tr","xyz");
 		pl.addDoctor(d);
 		pl.addDoctor(d1);
-
-		System.out.println("pl.getDoctors().get(0).getName() == murat  : "+(pl.getDoctors().get(0).getName()=="murat"));
-		System.out.println("pl.getDoctors().get(1).getName() == murat2 : " + (pl.getDoctors().get(1).getName()=="murat2"));
-		System.out.println("pl.getDoctors().size(), == 2               : "+(pl.getDoctors().size()==2));
-		System.out.println("pl.findDoctor(murat3, xyz) == -1           : "+(pl.findDoctor("murat3", "xyz")==-1));
-		System.out.println("pl.findDoctor(murat2, xyz) == 1            : "+(pl.findDoctor("murat2", "xyz")== 1));
-		System.out.println("pl.deleteDoctor(murat3, xyz) == null       : "+(pl.deleteDoctor("murat3", "xyz") == null));
-		System.out.println("pl.deleteDoctor(murat, xyz) == d           : "+(pl.deleteDoctor("murat", "xyz") == d));
-		System.out.println("pl.getDoctors().size(), == 1               : "+(pl.getDoctors().size()==1));
+		assertEquals(pl.getDoctors().get(0).getName(),"murat");
+		assertEquals(pl.getDoctors().get(1).getName(),"murat2");
+		assertEquals(pl.getDoctors().size(),2);
+		assertEquals(pl.findDoctor("murat3", "xyz"),-1);
+		assertEquals(pl.findDoctor("murat2", "xyz"),1);
+		assertNull(pl.deleteDoctor("murat3", "xyz"));
+		assertSame(pl.deleteDoctor("murat", "xyz"), d);	
+		assertEquals(pl.getDoctors().size(),1);
     }
 	/**
 	 * Nurse operating test
 	 */
-	//@Test
+	@Test
     static void testNurseOperating() {
-		System.out.println("\n******Nurse operation Unit test**********");
 		Polyclinic pl = new Polyclinic();
 		Nurse n1 = new Nurse("try","xyz","1",25,"xy1z@gtu.uedu.tr","xyz");
 		Nurse n2 = new Nurse("try2","xyz","1",25,"xyz@gtu.uedu.tr","xyz");
 		pl.addNurse(n1);
 		pl.addNurse(n2);
-		System.out.println("pl.getNurses().get(0).getSurname() == xyz  : "+(pl.getNurses().get(0).getSurname() == "xyz"));
-		System.out.println("pl.getNurses().get(1).getName(),try2)      : "+(pl.getNurses().get(1).getName()=="try2"));
-		System.out.println("pl.getNurses().size() == 2                 : " + (pl.getNurses().size() == 2));
-		System.out.println("pl.findNurse(try3, xyz) == -1              : "+(pl.findNurse("try3", "xyz") ==-1));
-		System.out.println("pl.findNurse(try2, xyz)==1                 : "+(pl.findNurse("try2", "xyz")==1));
-		System.out.println("pl.deleteNurse(tryx, xyz) == null          : "+(pl.deleteNurse("tryx", "xyz") == null));
-		System.out.println("pl.deleteNurse(try, xyz) == n1             : "+(pl.deleteNurse("try", "xyz") == n1));
-		System.out.println("pl.getNurses().size() == 1                 : " + (pl.getNurses().size() == 1));
+		assertEquals(pl.getNurses().get(0).getSurname(),"xyz");
+		assertEquals(pl.getNurses().get(1).getName(),"try2");
+		assertEquals(pl.getNurses().size(),2);
+		assertEquals(pl.findNurse("try3", "xyz"),-1);
+		assertEquals(pl.findNurse("try2", "xyz"),1);
+		assertNull(pl.deleteNurse("tryx", "xyz"));
+		assertSame(pl.deleteNurse("try", "xyz"), n1);	
+		assertEquals(pl.getNurses().size(),1);
     }
 	/**
 	 * secretary operating test
 	 */
-	//@Test
+	@Test
     static void testSecreterOperating() {
-		System.out.println("\n******Secretary operation Unit test**********");
 		Polyclinic pl = new Polyclinic();
 		Secreter s1 = new Secreter("secreter1","xyz","1",25,"xy1z@gtu.uedu.tr","xyz");
 		Secreter s2 = new Secreter("secreter2","xyz","1",25,"xyz@gtu.uedu.tr","xyz");
 		pl.addSecreter(s1);
 		pl.addSecreter(s2);
-
-		System.out.println("pl.getSecreters().get(0).getSurname() == xyz    : "+(pl.getSecreters().get(0).getSurname() =="xyz"));
-		System.out.println("pl.getSecreters().get(1).getName() == secreter2 : "+(pl.getSecreters().get(1).getName() == "secreter2"));
-		System.out.println("pl.getSecreters().size()==2                     : " + (pl.getSecreters().size()==2));
-		System.out.println("pl.findSecreter(secreterll, xyz) == -1          : "+(pl.findSecreter("secreterll", "xyz")==-1));
-		System.out.println("pl.findSecreter(secreter1, xyz) == 0            : "+(pl.findSecreter("secreter1", "xyz")==0));
-		System.out.println("pl.deleteSecreter(tryx, xyz)) == null           : "+(pl.deleteSecreter("tryx", "xyz") == null));
-		System.out.println("pl.deleteSecreter(secreter2, xyz)) == s2        : "+(pl.deleteSecreter("secreter2", "xyz") == s2));
-		System.out.println("pl.getSecreters().size()==1                     : " + (pl.getSecreters().size()==1));
+		assertEquals(pl.getSecreters().get(0).getSurname(),"xyz");
+		assertEquals(pl.getSecreters().get(1).getName(),"secreter2");
+		assertEquals(pl.getSecreters().size(),2);
+		assertEquals(pl.findSecreter("secreterll", "xyz"),-1);
+		assertEquals(pl.findSecreter("secreter1", "xyz"),0);
+		assertNull(pl.deleteSecreter("tryx", "xyz"));
+		assertNotNull(pl.deleteSecreter("secreter2", "xyz"));	
+		assertEquals(pl.getSecreters().size(),1);
     }
 	/**
 	 * patient operating test
 	 */
-	//@Test
+	@Test
     static void testPatientOperating() {
-		System.out.println("\n******Patient operation Unit test**********");
 		Polyclinic pl = new Polyclinic();
 		Patient p1 = new Patient("patient1","xyzw","1",25);
 		Patient p2 = new Patient("patient2","xyz","2",25);
 		pl.addPatients(p1);
 		pl.addPatients(p2);
-
-		System.out.println("pl.getPatients().peek().getSurname() == xyzw  : "+(pl.getPatients().peek().getSurname()=="xyzw"));
-		System.out.println("pl.getPatients().contains(p2) == true         : "+(pl.getPatients().contains(p2)==true));
-		System.out.println("pl.getPatients().size()==2                    : " + (pl.getPatients().size()==2  ));
-		System.out.println("pl.findPatient(patient21, xyz) == -1          : "+(pl.findPatient("patient21", "xyz")==-1));
-		System.out.println("pl.findPatient(patient1, xyzw) == 0           : "+(pl.findPatient("patient1", "xyzw") == 0 ));
-		System.out.println("pl.deletePatient(tryx, xyz) == null           : "+(pl.deletePatient("tryx", "xyz") == null));
-		System.out.println("pl.deletePatient(patient2, xyz) == null       : "+(pl.deletePatient("patient2", "xyz") != null));
-		System.out.println("pl.getPatients().size()==1                    : " + (pl.getPatients().size()==1));
+		assertEquals(pl.getPatients().peek().getSurname(),"xyzw");
+		assertEquals(pl.getPatients().contains(p2),true);
+		assertEquals(pl.getPatients().size(),2);
+		assertEquals(pl.findPatient("patient21", "xyz"),-1);
+		assertEquals(pl.findPatient("patient1", "xyzw"),0);
+		assertNull(pl.deletePatient("tryx", "xyz"));
+		assertNotNull(pl.deletePatient("patient2", "xyz"));	
+		assertEquals(pl.getPatients().size(),1);
     }
 	/**
 	 * Add method running time for Doctor,Nurse and scretary
@@ -122,22 +117,18 @@ public class ut_polyclinic {
 		if(temp != null)
 			System.out.println("running time for deleting "+deleting_index+".th element in the {"+ elementsize+"} Elements(doctor,nurse,secretary) : " + (System.nanoTime() - startTime )+ " ns");
 	}
-
-	/**
-	 * This function is used to test the running time of the add and delete methods
-	 * @param args The arguments passed to the program.
-	 */
 	public static void main(String [] args) {
+		
 		testDoctorOperating();
 		testNurseOperating();
 		testSecreterOperating();
 		testPatientOperating();
-		System.out.println("\n*************Adding operation running time************");
+		System.out.println("*************Adding operation running time************");
 		addMethodRunningTime(10);
 		addMethodRunningTime(100);
 		addMethodRunningTime(1000);
 		addMethodRunningTime(10000);
-		System.out.println("\n************Deleting operation running time**********");
+		System.out.println("************Deleting operation running time**********");
 		deleteMethodRunningTime(10,1);
 		deleteMethodRunningTime(100,1);
 		deleteMethodRunningTime(1000,1);
--- Hospital_Management_System/src/users/Doctor.java
+++ Hospital_Management_System/src/users/Doctor.java
@@ -6,11 +6,8 @@ import java.util.Date;
 import java.util.PriorityQueue;
 import java.util.UUID;
 
-import departments.Polyclinic;
 import systems.Appointment;
 import systems.Prescription;
-import tests.BloodTest;
-import tests.Test;
 
 
 
@@ -30,11 +27,6 @@ public class Doctor extends Worker
         // HMSystem.appointmets.add(appointment);
     }
 
-    /**
-     * This function displays the patient's prescriptions, doctors, and records
-     * 
-     * @param patient The patient object that is being displayed.
-     */
     public void displayPatientInfo(Patient patient) 
     {
         patient.displayPrescriptions();
@@ -49,23 +41,7 @@ public class Doctor extends Worker
         // HMSystem.addAppointmentToDataBase(app);
         return true;
     }
-
-    /**
-     * This function returns the first appointment in the queue 
-     * @return The appointment at the top of the stack.
-     */
-    public Appointment getAppointment()
-    {
-        return appointments.peek();
-    }
     
-    /**
-     * This function takes a patient, a prescription and a note as parameters and adds the prescription
-     * to the patient's list of prescriptions
-     * @param patient Patient object
-     * @param pres Prescription object
-     * @param note String
-     */
     public void givePrescription(Patient patient, Prescription pres , String note) {
         
         pres.setNote(note);
@@ -73,13 +49,6 @@ public class Doctor extends Worker
         //HMSystem.addAppointmentToDataBase(appo);
     }
    
-    /**
-     * It sets the note of the appointment, sets the discharged status to true, removes the appointment
-     * from the queue and adds it to the database
-     * 
-     * @param appo Appointment
-     * @param note String
-     */
     public void dischargePatient(Appointment appo , String note) {
         appo.setNote(note);
         appointments.peek().setDischarged(true);
@@ -87,32 +56,7 @@ public class Doctor extends Worker
         //HMSystem.addAppointmentToDataBase(appo);
     }
 
-    /**
-     * > This function adds a test to a patient
-     * 
-     * @param patient The patient object that is requesting the test.
-     * @param test The test to be requested
-     */
-    public void requestTest(Patient patient, Test test)
-    {
-        patient.addTest(test);
-    }
 
-    /**
-     * This function prints the names of all the patients who have an appointment with the doctor
-     */
-    public void waitingPatients() {
-        for (Appointment item : appointments) {
-            System.out.println(item.getPatient().getName());
-        }
-    }
-    
-    /**
-     * If the object is the same object, or if the object is the same type and has the same id, then
-     * the objects are equal
-     * 
-     * @param obj The object to compare with.
-     */
     @Override
     public boolean equals(Object obj) 
     {
@@ -133,76 +77,14 @@ public class Doctor extends Worker
         return true;
     }
 
-    /**
-     * It creates a doctor, adds an appointment to it, displays the patient's info, gives a
-     * prescription to the patient, requests a test from the patient, discharges the patient, adds 100,
-     * 1000 and 10000 appointments to the doctor and displays the time it takes to add them
-     */
     public static void main(String[] args) 
     {
-        System.out.print("\n\n\t\t\t DOCTOR UNIT TESTING STARTS \n\n");
         Doctor dr1 = new Doctor("name", "surname", "id", 30, "email", "password");
         System.out.print("\n");
-        System.out.print("Doctor 1 created = "+dr1 + "\n");
-
-        PriorityQueue<Appointment> appoints = new PriorityQueue<>();
-        Appointment appo1 = new Appointment(new Polyclinic("Radiology",7), dr1, new Patient("name", "surname", "id", 30), new Date(), "11111");
-        System.out.println("\nAppointment 1 created = "+appo1);
-        dr1.addAppointment(appo1);
-        System.out.println("\nAppointment 1 added to doctor 1 = "+dr1.appointments);
-
-        dr1.displayPatientInfo(dr1.getAppointment().getPatient());
-        System.out.println("\nFirst patient at appointment list is shown = "+dr1.getAppointment().getPatient());
-
-        dr1.givePrescription(dr1.getAppointment().getPatient(), new Prescription("newMed", "newInst", "newNote"), "Additional note");
-        System.out.println("\nAdded Prescription to the patient at the top of the list = "+dr1.getAppointment().getPatient().displayPrescriptions());
-        
-        dr1.requestTest(dr1.getAppointment().getPatient(), new BloodTest());
-        System.out.println("\nBloodtest requested from patient = "+dr1.getAppointment().getPatient().getTests());
-
-        dr1.dischargePatient(appo1, "note");
-        System.out.println("\nPatient Discharged = "+dr1.appointments);
-
-        long start = System.nanoTime(); 
-        for(int i = 0; i < 100; i++)
-        {
-            appo1 = new Appointment(new Polyclinic("Radiology",7), dr1, new Patient("name"+ i, "surname" + i, "id" + i, 30), new Date(), "11111" + i);
-            dr1.addAppointment(appo1);
-        }
-        long end1 = System.nanoTime(); 
-        long sum = (end1 - start);
-        
-        System.out.println("\nAdded 100 appointments to doctor 1 = " + sum/1000 + "ns");
-
-        dr1.appointments.clear();
-
-        start = System.nanoTime(); 
-        for(int i = 0; i < 1000; i++)
-        {
-            appo1 = new Appointment(new Polyclinic("Radiology",7), dr1, new Patient("name"+ i, "surname" + i, "id" + i, 30), new Date(), "11111" + i);
-            dr1.addAppointment(appo1);
-        }
-        end1 = System.nanoTime(); 
-        sum = (end1 - start);
-        
-        System.out.println("\nAdded 1000 appointments to doctor 1 = " + sum/1000 + "ns");
-
-        dr1.appointments.clear();
-
-        start = System.nanoTime(); 
-        for(int i = 0; i < 10000; i++)
-        {
-            appo1 = new Appointment(new Polyclinic("Radiology",7), dr1, new Patient("name"+ i, "surname" + i, "id" + i, 30), new Date(), "11111" + i);
-            dr1.addAppointment(appo1);
-        }
-        end1 = System.nanoTime(); 
-        sum = (end1 - start);
-        
-        System.out.println("\nAdded 10000 appointments to doctor 1 = " + sum/1000 + "ns");
-        
-        
-        System.out.print("\n\n\t\t\t DOCTOR UNIT TESTING ENDS \n\n");
-
+        System.out.print(dr1);
     }
 
+    public void waitingPatients() {
+    }
+    
 }
