31c8bebb2c2ac7473d14e731e0c82b3750413bb6
YeTeA <yusuftalhaaltun@gmail.com>
2022-05-07 00:55:45 +0300

    Revert "update"
    This reverts commit 2e0a14018a7b37a328d4ae31cacce92456e41101.

--- Hospital_Management_System/src/users/Secreter.java
+++ Hospital_Management_System/src/users/Secreter.java
@@ -1,5 +1,9 @@
 package users;
 
+import javax.print.Doc;
+
+import org.junit.platform.console.shadow.picocli.CommandLine.Spec;
+
 import systems.Prescription;
 
 /**
@@ -30,5 +34,67 @@ public class Secreter extends Worker{
      */
     public void displayQueue(Doctor doctor){
         doctor.waitingPatients();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("\n\n\n------------ Testing Secreter------------\n");
+        
+        String name = "name";
+        String surname = "surname";
+        String id = "id";
+        int age = 30;
+        String email = "email";
+        String password = "password";
+        Doctor doc = new Doctor(name, surname, id, age, email, password);
+        Patient patient = new Patient(name, surname, id, age);
+        Prescription pres = new Prescription("medicine", "instruction", "note");
+
+        long startTime = System.nanoTime();
+        testPrescribePatient(100, patient, pres, "note");
+        long endTime = System.nanoTime();
+        System.out.println("Running time for prescribing to a patient in the 100 elements : " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        testPrescribePatient(1000, patient, pres, "note");
+        endTime = System.nanoTime();
+        System.out.println("Running time for prescribing to a patient in the 1000 elements : " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        testPrescribePatient(10000, patient, pres, "note");
+        endTime = System.nanoTime();
+        System.out.println("Running time for prescribing to a patient in the 10000 elements : " + (endTime - startTime) + " ns");
+
+
+
+        startTime = System.nanoTime();
+        testDisplayQueue(100, doc);
+        endTime = System.nanoTime();
+        System.out.println("Running time for displaying queue in the 100 elements : " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        testDisplayQueue(1000, doc);
+        endTime = System.nanoTime();
+        System.out.println("Running time for displaying queue in the 1000 elements : " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        testDisplayQueue(10000, doc);
+        endTime = System.nanoTime();
+        System.out.println("Running time for displaying queue in the 10000 elements : " + (endTime - startTime) + " ns");
+
+    }
+    public static void testPrescribePatient(int counter, Patient patient, Prescription pres, String note){
+        Secreter sec = new Secreter("name", "surname", "id", 30, "email", "password");
+        for(int i = 0 ; i < counter ; i++){
+            sec.prescribePatient(patient, pres, note);
+        }
+
+    }
+
+    public static void testDisplayQueue(int counter, Doctor doctor){
+        Secreter sec = new Secreter("name", "surname", "id", 30, "email", "password");
+        for(int i = 0 ; i < counter ; i++){
+            sec.displayQueue(doctor);
+        }
     }
 }
