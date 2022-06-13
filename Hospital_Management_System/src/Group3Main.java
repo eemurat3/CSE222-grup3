43c5f469baafd411072ef25f08bf2ca6d26d8d31
YeTeA <yusuftalhaaltun@gmail.com>
2022-06-13 03:58:04 +0300

    Tests objects updated

--- Hospital_Management_System/src/Group3Main.java
+++ Hospital_Management_System/src/Group3Main.java
@@ -43,21 +43,23 @@ public class Group3Main {
         Date date = new Date();
 
 
-        Doctor doctor = new Doctor(name, surname, "1", age++, email, password);
-        Nurse nurse = new Nurse(name, surname, id, age++, email, password);
-        Technician technician = new Technician(name, surname, id, age++," username", password);
-        Patient patient = new Patient(name, surname, id, age++);
-        Secreter secreter = new Secreter(name, surname, id, age++, email, password);
-        Advisor advisor = new Advisor(name, surname, id, age++, "username", password);
+        Doctor doctor = new Doctor("doctorName", "doctorSurname", "1", age++, email, password);
+        Nurse nurse = new Nurse("nurseName", "nurseSurname", id, age++, email, password);
+        Technician technician = new Technician("technicianName", "technicianSurname", id, age++," username", password);
+        Patient patient = new Patient("patientName", "patientSurname", id, age++);
+        Patient patient1 = new Patient("patientName1", "patientSurname1", id, age++);
+        Secreter secreter = new Secreter("secreterName", "secreterSurname", id, age++, email, password);
+        Advisor advisor = new Advisor("advisorName", "advisorSurname", id, age++, "username", password);
         advisor.setDepartment(new Polyclinic("departmentName", 0));
 
-        Manager manager = new Manager(name, surname, id, age++, email, password);
+        Manager manager = new Manager("managerName", "managerSurname", id, age++, email, password);
 
         Polyclinic polyclinic = new Polyclinic("departmentName", 0);
         Lab lab = new Lab("departmentName", 1);
         Reception reception = new Reception("departmentName", 2);
 
         Appointment appointment = new Appointment(polyclinic, doctor, patient, date);
+        Appointment appointment1 = new Appointment(polyclinic, doctor, patient1, date);
         Prescription prescription = new Prescription("prescID", patient.getId(), "Med", "Inst", "newNote");
         
         CovidTest covidTest = new CovidTest(patient.getId(), testID);
@@ -66,19 +68,25 @@ public class Group3Main {
 
 
         doctor.addAppointment(appointment);
+        doctor.addAppointment(appointment1);
         doctor.setSecreter(secreter);
         doctor.setDepartment(polyclinic);
 
         patient.addAppointment(appointment);
         patient.addPrescription(prescription);
         patient.addTest(covidTest);
-        
 
+        patient1.addAppointment(appointment1);
+        patient1.addPrescription(prescription);
+        patient1.addTest(covidTest);
+        
+        
         secreter.addPrescription(prescription);
 
         polyclinic.addDoctor(doctor);
         polyclinic.addNurse(nurse);
         polyclinic.addPatients(patient);
+        polyclinic.addPatients(patient1);
         polyclinic.addSecreter(secreter);
 
         lab.addClinicalTechnician(technician);
@@ -88,6 +96,7 @@ public class Group3Main {
 
         reception.addAdvisorStaff(advisor);
         reception.addAppointment(appointment);
+        reception.addAppointment(appointment1);
 
         HMSystem myHospital = new HMSystem();
 
@@ -107,10 +116,13 @@ public class Group3Main {
         myHospital.users.add(advisor);
         myHospital.users.add(secreter);
         myHospital.users.add(patient);
+        myHospital.users.add(patient1);
 
         myHospital.patients.add(patient);
+        myHospital.patients.add(patient1);
 
         myHospital.appointments.add(appointment);
+        myHospital.appointments.add(appointment1);
 
         myHospital.tests.add(covidTest);
         myHospital.tests.add(bloodTest);
